ceeb3ca9b708b724d70043fa3fdb542095e58ba7
Ibna Masud Rana Parvej <masud1603056@gmail.com>
2024-05-01 00:35:12 +0600

    generate project using spring initializr

---
+++ movieList/src/main/java/Movie/list/movieListImplement/a/simple/movie/listing/application/MovieListApplication.java
@@ -0,0 +1,13 @@
+package Movie.list.movieListImplement.a.simple.movie.listing.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieListApplication.class, args);
+	}
+
+}
---
+++ movieList/src/test/java/Movie/list/movieListImplement/a/simple/movie/listing/application/MovieListApplicationTests.java
@@ -0,0 +1,13 @@
+package Movie.list.movieListImplement.a.simple.movie.listing.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
