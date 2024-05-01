f6e037df21deabcee319b0e51a892077edc2da7b
Ibna Masud Rana Parvej <masud1603056@gmail.com>
2024-05-01 17:17:12 +0600

    Adding search for either title, cast or category

--- movieList/src/main/java/Movie/list/movieListImplement/a/simple/movie/listing/application/setup/controller/MovieController.java
+++ movieList/src/main/java/Movie/list/movieListImplement/a/simple/movie/listing/application/setup/controller/MovieController.java
@@ -1,18 +1,20 @@
 package Movie.list.movieListImplement.a.simple.movie.listing.application.setup.controller;
-
 import Movie.list.movieListImplement.a.simple.movie.listing.application.setup.entiry.Movie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class MovieController {
 
     private static final List<Movie> movies = new ArrayList<>();
 
+    //adding demo movies
     static {
         movies.add(new Movie("12th Fail", "Vikrant Massey, Medha Shankr", "Love & Drama", 2023,1810683.20));
         movies.add(new Movie("777 Charlie", "Rakshit Shetty", " Adventure Drama", 2022, 2820683.50));
@@ -21,9 +23,22 @@ public class MovieController {
         movies.add(new Movie("Pulp Fiction", "John Travolta, Uma Thurman", "Crime", 1994, 850000.60));
     }
 
+    //Searching all the movies in the app.
     @GetMapping("/movies")
     public ResponseEntity<List<Movie>> getAllMovies() {
         return ResponseEntity.ok(movies);
     }
 
+    //searching for either title, cast or category
+    @GetMapping("/movies/search")
+    public ResponseEntity<List<Movie>> searchMovies(
+            @RequestParam(value = "query", required = true) String query) {
+        List<Movie> searchResults = movies.stream()
+                .filter(movie -> movie.getTitle().toLowerCase().contains(query.toLowerCase()) ||
+                        movie.getCast().toLowerCase().contains(query.toLowerCase()) ||
+                        movie.getCategory().toLowerCase().contains(query.toLowerCase()))
+                .sorted(Comparator.comparing(Movie::getTitle))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(searchResults);
+    }
 }
